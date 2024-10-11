632803a001f9f33d60591730b83c7f0c130eb3f8
Mathews Dantas <94187504+MathewsDantas@users.noreply.github.com>
2024-10-10 22:32:16 -0300

    feat: Adicionar importação/exportação de dados para os modelos Pedido e Usuario

--- pedido/admin.py
+++ pedido/admin.py
@@ -1,17 +1,19 @@
 from django.contrib import admin
 from import_export import resources
-
+from import_export.admin import ImportExportModelAdmin
 
 from .models.pedido import Pedido
 
 
+class PedidoResource(resources.ModelResource):  # serve para importar e exportar dados
+    class Meta:
+        model = Pedido
+        fields = ("id", "interno", "feito_por", "data", "status", "valor_total")
+
+
 @admin.register(Pedido)
-class PedidoAdmin(admin.ModelAdmin):
+class PedidoAdmin(ImportExportModelAdmin):
+    resource_class = PedidoResource
     list_display = ["id", "interno", "feito_por", "data", "status", "valor_total"]
     search_fields = ["feito_por", "status"]
     list_filter = ["status"]
-
-
-class PedidoResource(resources.ModelResource):
-    class Meta:
-        model = Pedido
--- usuario/admin.py
+++ usuario/admin.py
@@ -1,11 +1,19 @@
 from django.contrib import admin
-
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
 
 from .models import Usuario, Pessoa
 
 
+class UsuarioResource(resources.ModelResource):
+    class Meta:
+        model = Usuario
+        fields = ("id", "email", "is_active", "is_staff", "is_superuser")
+
+
 @admin.register(Usuario)
-class UsuarioAdmin(admin.ModelAdmin):
+class UsuarioAdmin(ImportExportModelAdmin):
+    resource_class = UsuarioResource
     list_display = ["id", "email", "is_active", "is_staff", "is_superuser"]
     list_filter = ["is_active", "is_staff", "is_superuser"]
     search_fields = ["email"]
